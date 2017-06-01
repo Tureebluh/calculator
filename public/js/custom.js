4a71acf60c55d4670af10eac854366a7ec78ae11
Justin <yessdrillsgt@yahoo.com>
2017-06-01 19:51:01 -0400

    Changed input type='button' to button elements
    Using javascript's eval function to evaluate the string

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,108 +1,55 @@
 $(document).ready(function(){
 	// Declare global variables
 	var currentVal = '';
-	var lastVal = '';
-	var lastOperator = '';
 	const ERROR = 'error';
-	
+	var lastVal;
 	
 	$('.numbers').on('click', function(){
 		currentVal += $(this).val();
+		lastVal += $(this).val();
 		$('#result').val(currentVal);
-		
-		Get_globalValues('after numbers on click');
 	});
 	
 	
 	$('.clear').on('click', function(){
+		var temp = '';
 		
 		switch($(this).val()){
 			case "AC":
 				ResetCalculator();
 				break;
 			case "CE":
-				currentVal = '';
-				$('#result').val('');
+				// remove lastVal from currentVal
+				for (var i = 0; i < currentVal.length - lastVal.length; i++){
+					temp += currentVal[i];
+				}
+				
+				currentVal = temp;
+				$('#result').val(currentVal);
 				break;
 		}
-		
-		Get_globalValues('after clear on click');
 	});
 	
-	
-	// Perform arithmetic operations 
+	 
 	$('.arithmetic').on('click', function(){
-		Get_globalValues('before arithmetic on click');
-		
-		
-		if (currentVal !== ''){
-			if (lastOperator.length > 0){ // this is at least the second operation performed
-				lastVal = PerformOperation(lastVal, currentVal, lastOperator); // Update lastVal  
-				
-			} else { // first operation performed
-				lastVal = currentVal; // Capture the current value	
-			}
-			
-			currentVal = ''; // Reset currentVal
-			lastOperator = $(this).val(); // Capture the operator used
-			$('#result').val(''); // Reset the result textbox
-		
-		} else { // no results have been input yet
-			$('#result').val(ERROR);
-			ResetCalculator();
-		}
-		
-		Get_globalValues('after arithmetic on click');
+		currentVal += $(this).val();
+		lastVal = $(this).val();
+		$('#result').val(currentVal);
 	});
 	
 	
 	$('#equal').on('click', function(){
-		if (lastVal !== '' && currentVal !== '' && lastOperator !== ''){
-			lastVal = PerformOperation(lastVal, currentVal, lastOperator);
-			lastOperator = '';
-			$('#result').val( lastVal );
-			currentVal = lastVal;
-			
+		if ($.isNumeric(eval(currentVal))){
+			$('#result').val(eval(currentVal));
 		} else {
 			$('#result').val(ERROR);
-			ResetCalculator();
 		}
-		
-		Get_globalValues('after equal on click');
 	});
-	
-	
-	function PerformOperation(lastVal, currentVal, operation){
-		switch(operation){
-			case '+':
-				return parseFloat(lastVal) + parseFloat(currentVal);
-				break;
-			case '-':
-				return parseFloat(lastVal) - parseFloat(currentVal);
-				break;
-			case '*':
-				return parseFloat(lastVal) * parseFloat(currentVal);
-				break;
-			case '/':
-				return parseFloat(lastVal) / parseFloat(currentVal);
-				break;
-			default:
-				$('#result').val(ERROR);
-				ResetCalculator();
-				break;
-		}
-	};
 	
 	
 	function ResetCalculator(){
 		currentVal = '';
-		lastVal = '';
-		lastOperator = '';
 		$('#result').val('');
 	};
-	
-	
-	function Get_globalValues(whereFrom){
-		console.log('whereFrom = ' + whereFrom + ' currentVal = ' + currentVal + ' lastVal = ' + lastVal + ' lastOperator = ' + lastOperator);
-	};
+
 });
